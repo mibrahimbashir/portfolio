bc4c5e9e621c0825802deb5d5fa48913db9c464a
ibrahim bashir <ibrahimbashir370@gmail.com>
2024-09-12 16:52:42 +0500

    Process contact form submission.

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path("project-1/", views.project_1, name="project-1"),
 	path("project-2/", views.project_1, name="project-2"),
 	path("project-3/", views.project_1, name="project-3"),
+	path("thank-you/", views.contact_success, name="thank-you"),
 ]
--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import redirect, render
-from django.core.exceptions import ValidationError
-from django.core.validators import validate_email
 from .models import Messages
+import re
 
 # Create your views here.
 
-def home(request):
-	context = {}
-	return render(request, 'index.html', context)
+def validate_email(email):
+    # Define the regex pattern for email
+    pattern = r"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+    
+    # Use re.match to check if the email matches the pattern
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+
+def home(request):      
+	if request.method == 'POST':
+		name = request.POST.get('name')
+		email = request.POST.get('email')
+		message = request.POST.get('message')
+
+		Messages.objects.create(name=name, email=email, message=message)
+		return redirect('thank-you')
+
+	return render(request, 'index.html')
 
 def project_1(request):
 	return render(request, 'project-1.html')
+
+def contact_success(request):
+	return render(request, 'thank-you-page.html')
